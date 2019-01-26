2123c5e8e2e4cd90daf694d72e1ec8378da3a6b1
bargoss <baranyesiltepe98@gmail.com>
2019-01-27 02:46:03 +0300

    skybox shader done

--- AngelAssignment4/fSkybox.glsl
+++ AngelAssignment4/fSkybox.glsl
@@ -5,6 +5,7 @@ in vec2 TexCoord;
 in vec3 Normal;
 in vec3 FragPos;
 
+uniform mat4 model;
 out vec4 color;
 
 struct Light
@@ -112,6 +113,14 @@ float remap( float minval, float maxval, float curval )
     return ( curval - minval ) / ( maxval - minval );
 } 
 //                       r      b    g
+vec4 rgb(float r, float b, float g){
+	return vec4(r/256, b/256, g/256, 1);
+}
+
+const vec4 sun = rgb(230, 251, 95); 
+const vec4 blueLight = rgb(153, 219, 255);
+const vec4 blueDark = rgb(13, 112, 165);
+const vec4 night = rgb(3, 38, 57);
 
 void main()
 {
@@ -119,5 +128,33 @@ void main()
 	//finalColor += CalcPointLights();
 	finalColor += CalcSpotLight(spotLight);
 
-	color = texture(theTexture, TexCoord) * finalColor;	
+	//color = texture(theTexture, TexCoord) * finalColor;	
+
+	
+	//color = vec4(vec3(FragPos) * 0.02,1);
+	//return;
+	
+	vec4 up = model[1];
+	vec4 camPos = model[3];
+	vec4 current = normalize(vec4(vec3(FragPos - vec3(model[3])),1));
+	
+	float skyFloat =  dot(up,current);
+	//skyFloat = skyFloat * skyFloat;
+
+	if(skyFloat > 0.9){
+		float ratio = remap(0.9, 1.0, skyFloat);
+		color = ratio * sun + (1 - ratio) * blueLight;
+	}
+	else if(skyFloat > 0.2){
+		float ratio = remap(0.2, 0.9, skyFloat);
+		color = ratio * blueLight + (1 - ratio) * blueDark;
+	}
+	else if(skyFloat > -0.6){
+		float ratio = remap(-0.6, 0.2, skyFloat);
+		color = ratio * blueDark + (1 - ratio) * night;
+	}
+	else{
+		color = night;
+	}
+
 }
